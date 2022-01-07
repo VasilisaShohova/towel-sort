e07efedca166cd7c13f56529b5a787166a3979bf
Vasilisa Shokhava <vasilisa.shokhava@wolterskluwer.com>
2022-01-07 22:24:04 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined) {
+    return [];
+  }
+ if (matrix.length === 0) {
+   return [];
+ }
+  for (var i = 1; i < matrix.length; i++) {
+    let elArray = matrix[i];
+    if(i%2!==0){
+   elArray.reverse().join();
+    }
+  }
+  let matrixWithCorrectOrder = matrix.join().split(",").map(x=>+x)
+  return matrixWithCorrectOrder;
 }
